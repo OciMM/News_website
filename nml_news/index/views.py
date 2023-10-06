aab845549c6cb6b708a410c44a9ce1710279ae20
OciMM <bekasovmaks20@gmail.com>
2023-10-06 12:01:49 +0300

    перед добавкой ajax

--- nml_news/index/views.py
+++ nml_news/index/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+# импортирование из моделей других приложений
+from news.models import News
+from thoughts.models import Thoughts
+
 
 class IndexView(View):
     """Получение главной страницы"""
     def get(self, request):
-        return render(request, 'index/index.html')
+        news_list = News.objects.all()
+        thoughts_list = Thoughts.objects.all()
+        return render(request, 'index/index.html', {'news_list': news_list,
+                                                    'thoughts_list': thoughts_list})
+
